a0371b78b8aedcf1869ad2f019824ada0d67556f
REDzSEA <57469624+REDzTrue@users.noreply.github.com>
2021-05-07 08:14:51 +0700

    Update Vouchercode_class.php

--- Vouchercode_class.php
+++ Vouchercode_class.php
@@ -5,20 +5,17 @@
  * Original code by REDzTrue/REDzSEA
  * Response code by Kumihoaomkung
  * Truemoney wallet VoucherCode
- * New update GetPoint
- * 4/2/2021 Last Update
+ * 5/7/2021 Last Update
  * https://github.com/REDzTrue/TruemoneyWallet-VouncherCode/
  */
 
-class VoucherCode
-{
-    public function fetch($method = null, $url = null, $headers = array(), $data = null)
-    {
+class TMN {
+
+    public function fetch($method = null, $url = null, $headers = array(), $data = null) {
         $this->url = $url;
         $this->headers = $headers;
         $this->data = $data;
         $this->method = $method;
-        // ส่งข้อมูล
         $fetch = curl_init();
         $headers = ["Content-Type" => "application/json"];
         curl_setopt_array($fetch, [
@@ -31,15 +28,11 @@ class VoucherCode
             CURLOPT_HTTPHEADER => $this->buildHeaders($headers),
             CURLOPT_POSTFIELDS => $data
         ]);
-        // Reponse
         $this->response = curl_exec($fetch);
-        // จบการส่งข้อมูล
         curl_close($fetch);
-        // ส่งข้อมูลกลับ
         return $this->response;
     }
 
-    // Headers
     public function buildHeaders($array)
     {
         $headers = array();
@@ -49,7 +42,6 @@ class VoucherCode
         return $headers;
     }
 
-    // VoucherCode
     public function VoucherCode($Mobile = null, $voucher_code = null)
     {
         $this->Mobile = $Mobile;
@@ -60,38 +52,71 @@ class VoucherCode
         } else if ($this->empty($this->Mobile) == true) {
             return $this->error(307, "ไม่พบเบอร์โทรศัพน์ของผู้รับโปรดเเจ้งผู้ควบคุมเพื่อเเก้ไขปัญหานี้");
         } else {
+
             $this->gift = str_replace("https://gift.truemoney.com/campaign/?v=", "", $this->VoucherCode);
+
             if (strlen($this->gift) <= 0) {
                 return $this->error(306, "ลิงค์ของซองนี้ไม่ถูกต้อง กรุณาตรวจสอบลิงก์นี้อีกครั้ง");
             }
-            $res = json_decode($this->fetch("POST", "https://gift.truemoney.com/campaign/vouchers/{$this->gift}/redeem", null, json_encode(array("mobile" => $this->Mobile, "voucher_hash" => $this->VoucherCode))), true);
-            if ($res["status"]["code"] == "SUCCESS") {
-                $this->point = $res["data"]["voucher"]["redeemed_amount_baht"];
-                return $this->success(200, "คุณได้รับเงินจากซองนี้แล้ว", $this->point);
-            } elseif ($res['status']['code'] === "CANNOT_GET_OWN_VOUCHER") {
-                return $this->error(301, "คุณไม่สามารถใส่ซองของขวัญของคุณเองได้");
-            } elseif ($res['status']['code'] === "TARGET_USER_NOT_FOUND") {
-                return $this->error(302, "ไม่พบชื่อผู้ใช้เบอร์ Wallet นี้");
-            } elseif ($res['status']['code'] === "INTERNAL_ERROR") {
-                return $this->error(500, "ไม่มีซองนี้ในระบบ กรุณาตรวจสอบลิงก์นี้อีกครั้ง");
-            } elseif($res['status']['code'] === "VOUCHER_OUT_OF_STOCK"){
-                return $this->error(420, "ซองนี้ได้มีคนรับไปแล้ว");
-            } else {
-                return $res;
+
+            // ส่งข้อมูลซอง
+            $res = json_decode($this->fetch("POST"
+                                            , "https://gift.truemoney.com/campaign/vouchers/{$this->gift}/redeem"
+                                            , null
+                                            , json_encode(array("mobile" => $this->Mobile
+                                            , "voucher_hash" => $this->VoucherCode)))
+                                            , true);
+            
+            $stats = $res["status"]["code"];
+            
+            switch ($stats) {
+                case "SUCCESS":
+                    $this->point = $res["data"]["voucher"]["redeemed_amount_baht"];
+                    return $this->success(200, "คุณได้รับเงินจากซองนี้แล้ว", $this->point);
+                    break;
+
+                case "CANNOT_GET_OWN_VOUCHER":
+                    return $this->error(301, "คุณไม่สามารถใส่ซองของขวัญของคุณเองได้");
+                    break;
+                
+                case "TARGET_USER_NOT_FOUND":
+                    return $this->error(302, "ไม่พบชื่อผู้ใช้เบอร์ Wallet นี้");
+                    break;
+
+                case "INTERNAL_ERROR":
+                    return $this->error(500, "ไม่มีซองนี้ในระบบ กรุณาตรวจสอบลิงก์นี้อีกครั้ง");
+                    break;
+
+                case "VOUCHER_OUT_OF_STOCK":
+                    return $this->error(420, "ซองนี้ได้มีคนรับไปแล้ว");
+                    break;
+
+                case "VOUCHER_NOT_FOUND":
+                    return $this->error(700, "ไม่พบซองของขวัญ")
+                    break;
+                
+                case "VOUCHER_EXPIRED":
+                    return $this->error(705, "ซองของขวัญนี้หมดอายุแล้ว");
+                    break;
+
+                default:
+                    return $res;
+                    break;
             }
+
         }
     }
 
     public function GetPoint() {
-        return $this->response(["point"=>$this->point]);
+        return $this->response([ "point" => $this->point ]);
     }
 
     public function GetMobile() {
-        return $this->response(["phone"=>$this->Mobile]);
+        return $this->response([ "phone" => $this->Mobile ]);
     }
 
     public function GetGiftcode() {
-        return $this->response(["code"=>$this->gift]);
+        return $this->response([ "code" => $this->gift ]);
     }
 
     public function empty($data) {
@@ -100,18 +125,19 @@ class VoucherCode
         }
     }
 
-    public function response($array) {
-        return json_encode($array);
-    }
-
     public function success($code, $response, $amount) {
-        return json_encode(["code"=>$code, "message"=>$response, "amount"=>$amount]);
+        return json_encode([
+            "code"=>$code
+            , "message"=>$response
+            , "amount"=>$amount
+            ]);
     }
 
     public function error($code, $response) {
-        return json_encode(["code"=>$code, "message"=>$response]);
+        return json_encode([
+            "code"=>$code
+            ,"message"=>$response
+            ]);
     }
 
 }
-
-?>
